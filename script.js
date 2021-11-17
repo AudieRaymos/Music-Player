bbe46e978e31cd637f202cc1c88e9536cef0dc80
AudieRaymos <audriannakraymos@gmail.com>
2021-11-17 14:01:54 -0800

    begin js

---
+++ script.js
@@ -0,0 +1,9 @@
+const songs = [
+    "A-Figure-of-Speech_AdobeStock_357620408_preview.m4a",
+    "Acoustic-Live_AdobeStock_468477272_preview.m4a",
+    "Happy_AdobeStock_465136925_preview.m4a",
+    "Joyful-Party_AdobeStock_468476472_preview.m4a",
+    "Rainy-City-Nights_AdobeStock_465137747_preview.m4a",
+    "Summer-Upbeat-Rock_AdobeStock_387181392_preview.m4a",
+    "Sunset-Drive_AdobeStock_465141477_preview.m4a"
+];
